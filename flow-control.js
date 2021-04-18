a0b292a08dedbb8df026488ca820dd8bdf9411cf
Matt Price <matprc246@gmail.com>
2021-04-18 18:14:07 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,17 +1,27 @@
 function basicTeenager(age) {
-
+  if (age === '13-19') {
+    return `You are a teenager!`
+  }
 }
 
 function teenager(age) {
-
+  if (age === '13-19') {
+    `You are a teenager!`
+  } else {
+    `You are not a teenager!`
+  }
 }
 
 function ageChecker(age) {
-
+  if(age === '13-19') {
+    console.log('You are a teenager!');
+  } else {
+    console.log('You are a kid');
+  }
 }
 
 function ternaryTeenager(age) {
-
+  console.log('You' + (age === '15' ? 'are a teenager!' : 'are a kid'));
 }
 
 function switchAge(age) {
